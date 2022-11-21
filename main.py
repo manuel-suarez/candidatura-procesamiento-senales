858ded78b839458aa4364df48d3cd8924fc15dd4
Arturo Suárez <manuel.suarez@cimat.mx>
2022-11-21 09:38:15 -0600

    Definición de parámetros iniciales

--- main.py
+++ main.py
@@ -6,3 +6,34 @@ from matplotlib.ticker import StrMethodFormatter
 import numpy as np
 from numpy.random import random, randint, randn
 from zernike import RZern
+
+# Definimos parámetros iniciales
+scale_factor = 40   # Factor de escala para obtener el rango dinámico de los polinomios
+order = 6           # Orden de los polinomios a generar
+cart = RZern(order) # Generador de polinomios
+dim = 128           # Tamaño de dimensiones de imagen
+L, K = dim, dim     # Tamaño de cada imagen
+num = 20000         # Tamaño conjunto de entrenamiento
+num_test = 5000     # Tamaño conjunto de prueba
+num_epochs = 50     # Número de épocas para el entrenamiento
+latent_dim = 10      # Dimensión del espacio latente de la red VAE
+# Definimos el grid para la generación de las señales
+ddx = np.linspace(-1.0, 1.0, K)
+ddy = np.linspace(-1.0, 1.0, L)
+xv, yv = np.meshgrid(ddx, ddy)
+cart.make_cart_grid(xv, yv)
+# Generamos 30,000 señales variando los parámetros del polinomio a generar
+Z = []
+for i in range(num):
+  # Variamos los parámetros para el polinomio
+  c = np.random.normal(size=cart.nk)
+  # Generamos polinomio
+  Phi = cart.eval_grid(c, matrix=True)
+  # Reemplazamos NaN con 0
+  p = np.nan_to_num(Phi, False, 0)
+  #p = Phi
+  # Reescalamos a 0-1 (necesario para que la red calcule correctamente las métricas)
+  # Verificar con el Dr. si esto sería necesario en este caso
+  #p_scaled = (p - np.min(p)) / (np.max(p) - np.min(p))
+  # Agregamos a conjunto de resultados
+  Z.append(p)
