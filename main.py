6416f3cb675a6fcde40b629c14a599a0af5e5226
Arturo Suárez <manuel.suarez@cimat.mx>
2022-11-21 09:37:59 -0600

    Especificación de librerías

---
+++ main.py
@@ -0,0 +1,8 @@
+# Import
+import matplotlib.pyplot as plt
+from matplotlib import cm
+from matplotlib.ticker import LinearLocator
+from matplotlib.ticker import StrMethodFormatter
+import numpy as np
+from numpy.random import random, randint, randn
+from zernike import RZern
